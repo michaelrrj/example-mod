772ebc3fe2c82305b3b8dc474be031c52d8920ce
michaelrrj <162162672+michaelrrj@users.noreply.github.com>
2026-06-14 12:51:41 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,83 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+static int fakeDeaths = 0;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+class $modify(FakeDeathPlayLayer, PlayLayer) {
+    CCLabelBMFont* m_deathLabel = nullptr;
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    bool init(GJGameLevel* level) {
+        if (!PlayLayer::init(level))
+            return false;
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        m_deathLabel = CCLabelBMFont::create(
+            "Deaths: 0",
+            "goldFont.fnt"
+        );
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        m_deathLabel->setAnchorPoint({0.f, 1.f});
+        m_deathLabel->setPosition({8.f,
+            CCDirector::sharedDirector()
+                ->getWinSize().height - 8.f});
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        m_uiLayer->addChild(m_deathLabel, 999);
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        schedule(schedule_selector(FakeDeathPlayLayer::updateFakeDeaths));
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        return true;
+    }
+
+    void updateFakeDeaths(float) {
+        auto player = m_player1;
+
+        if (!player)
+            return;
+
+        auto pos = player->getPosition();
+
+        bool nearSpike = false;
+
+        auto objects = m_objects;
+
+        for (auto obj : CCArrayExt<GameObject*>(objects)) {
+
+            int id = obj->m_objectID;
+
+            bool hazard =
+                id == 8 ||
+                id == 39 ||
+                id == 103 ||
+                id == 392;
+
+            if (!hazard)
+                continue;
+
+            float dx =
+                obj->getPositionX() - pos.x;
+
+            float dy =
+                obj->getPositionY() - pos.y;
+
+            float dist =
+                sqrtf(dx * dx + dy * dy);
+
+            if (dist < 120.f) {
+                nearSpike = true;
+                break;
+            }
+        }
+
+        if (nearSpike) {
+            fakeDeaths++;
+
+            m_deathLabel->setString(
+                fmt::format(
+                    "Deaths: {}",
+                    fakeDeaths
+                ).c_str()
+            );
+        }
+    }
 };
